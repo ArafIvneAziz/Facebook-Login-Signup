e2cedc8b357dc0b28926e4c5c734a1c7875a03a3
yamin <yaminahad420@gmail.com>
2023-10-16 09:58:19 -0700

    first commit

---
+++ Login.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+import time
+
+# Create a WebDriver instance
+driver = webdriver.Chrome()
+
+# Open the Facebook login page
+driver.get('https://www.facebook.com')
+
+# Find the username and password input fields by name
+username_field = driver.find_element(By.NAME, 'email')
+password_field = driver.find_element(By.NAME, 'pass')
+
+# Enter your Facebook username and password
+username_field.send_keys('your-username')
+password_field.send_keys('your-email')
+
+# Find the login button by name
+login_button = driver.find_element(By.NAME, 'login')
+
+# Click the login button
+login_button.click()
+
+# Wait for a few seconds to ensure the login is complete
+time.sleep(10)  # You can adjust the sleep duration as needed
+
+# You are now logged in. Add any additional actions or navigate to different pages as needed.
+# next_page_html = driver.page_source
+# print(next_page_html)
+# Finally, don't forget to close the WebDriver when you're done
+driver.quit()
---
+++ README.md
@@ -0,0 +1,9 @@
+You Must need to install webdriver such as Chromedriver, EdgeDriver, etc.
+
+Then You need to install the latest verson of selenium:
+
+pip install selenium
+
+You can custom this code for specific tasks or sign up process
+
+it's ready for login
